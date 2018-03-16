8ab8121a1b6fcdc386337e56d10f772a46d13239
Profsoft <profsoft@profsofts-mac-mini.local>
2018-03-16 19:03:06 +0400

    Second homework

---
+++ SecondHomework/SecondHomework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecondHomework
+//
+//  Created by Profsoft on 16/03/2018.
+//  Copyright © 2018 Profsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecondHomework/SecondHomework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecondHomework
+//
+//  Created by Profsoft on 16/03/2018.
+//  Copyright © 2018 Profsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
